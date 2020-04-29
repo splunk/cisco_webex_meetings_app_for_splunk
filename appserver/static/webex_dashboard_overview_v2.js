88e896d09bb36221958b283ba7bce274c3b157bb
Philippe Tang <ptang@splunk.com>
2020-04-29 00:02:48 +0000

    Latest changes

--- appserver/static/webex_dashboard_overview_v2.js
+++
@@ -1,7 +0,0 @@
-require(['jquery', 'splunkjs/mvc/simplexml/ready!'], function($) {
-    var aRow = $('.dashboard-row')[0];
-    var aPanelCells = $(aRow).children('.dashboard-cell');
-    // Adjust the cells' width
-    $(aPanelCells[0]).css('width', '33.3%');
-    $(aPanelCells[1]).css('width', '66.6%');  
-});
